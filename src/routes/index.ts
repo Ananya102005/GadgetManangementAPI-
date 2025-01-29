f368beeeb2f19e7ff6557b12d6f27dc2bd8faab0
amogha r <amoghar23@gmail.com>
2025-01-29 23:47:45 +0530

    added dummy route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,14 @@ import express, { Router } from "express";
 import gadgetsRouter from "./gadgets";
 import authRouter from "./auth";
 import { authMiddleware } from "../middlewares/auth";
-const router : Router = express.Router();
+const router: Router = express.Router();
 
 router.use("/gadgets", authMiddleware, gadgetsRouter);
 router.use("/auth", authRouter);
-
+router.use("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Upraised Assignment API",
+  }); // dummy route to check if the server is running and
+    //  to keep the server alive as i am using render which has cold shutdown
+});
 export default router;
